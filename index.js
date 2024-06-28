7075495e49f19e212c98c9252eb111387734de03
ashutosh <agoyal@flipshope.com>
2024-06-29 00:02:15 +0530

    update

--- index.js
+++ index.js
@@ -16,13 +16,42 @@ const PORT = 3030;
 
 let connectedClients = [];
 
-io.on('connection', (socket) => {
-    console.log('A client connected:', socket.id);
-    connectedClients.push(socket);
+const store_min_version_support = {
+    1:1,
+    2:2,
+    3:999,
+    4:2,
+    5:2,
+    6:2,
+    7:2,
+    8:2,
+    9:2,
+    10:2,
+    11:2,
+    12:2,
+    13:2,
+    14:999,
+    15:999,
+    16:2,
+    17:999,
+    18:999,
+    19:999,
+    20:999,
+}
 
+io.on('connection', (socket) => {
+
+    const version = socket.handshake?.query?.version || 1;
+    console.log(`Client: ${socket.id} && version: ${version}`);
+    // console.log('A client connected:', socket.id);
+    socket.version = version;
+    connectedClients.push(socket);
+    // if(!connectedClients_wv.version) connectedClients_wv.version = [];
+    // connectedClients_wv.version.push(socket);
     socket.on('disconnect', () => {
         console.log('A client disconnected:', socket.id);
         connectedClients = connectedClients.filter(s => s.id !== socket.id);
+
     });
 
     socket.on('response', (data) => {
@@ -58,12 +87,16 @@ app.get('/scrap_socket/getData', (req, res) => {
     if (!sid || !pid) {
         return res.status(400).json({ error: 'Both sid and pid are required.' });
     }
-
+    let min_supp_v = store_min_version_support[sid];
     if (!connectedClients.length) {
         return res.json({ error: 'No clients connected' });
     }
-    const randomClientIndex = Math.floor(Math.random() * connectedClients.length);
-    const clientSocket = connectedClients[randomClientIndex];
+    let connectedEligibleClients = connectedClients.filter(s => s.version >= min_supp_v);
+    if (!connectedEligibleClients.length) {
+        return res.json({ error: 'No supported clients connected' });
+    }
+    const randomClientIndex = Math.floor(Math.random() * connectedEligibleClients.length);
+    const clientSocket = connectedEligibleClients[randomClientIndex];
     const requestId = 1234; // uidv4(); // Generate a unique request ID
 
     //const clientSocket = connectedClients[0]; // select a client, modify as necessary
